2b4aff1123e4cfa10e07628d6b604cee3420381c
JoseCarlosBC <62607068+JoseCarlosBC@users.noreply.github.com>
2020-04-13 21:15:50 -0600

    Add files via upload
    ahi te los arregle, no los pude sincronizar pero ahi te va para que lo mires jaja
    
    att: tu buen amigo el hombre araña

--- proceso.php
+++ proceso.php
@@ -1,25 +1,21 @@
 <?php
- $Valor1=$_POST['n1'];
- $Valor2=$_POST['n2'];
- $Operacion=$_POST['operacion'];
- $resultado;
- 
-  
-  if($Operacion =='suma')
-  {
-   $resultado=($Valor1 + $Valor2);
-  }
-   if($Operacion == 'resta')
-  {
-  $resultado=($Valor1 - $Valor2);
-  }
-   if($Operacion == 'multiplicación')
-  {
-  $resultado=($Valor1 * $Valor2);
-  }
-   if($Operacion == 'división')
-  {
-  $resultado=($Valor1 / $Valor2);
-  }
- 
+    $valor1= $_POST['n1'];
+    $valor2= $_POST['n2'];
+    $op=$_POST['operacion'];
+
+    if($op==1){
+        ECHO "la suma es: ".($valor1+$valor2);
+	}
+
+    if($op==2){
+        ECHO "la resta es: ".($valor1-$valor2);
+	}
+
+    if($op==3){
+        ECHO "la multiplicacion es: ".($valor1*$valor2);
+	}
+
+    if($op==4){
+        ECHO "la division es: ".($valor1/$valor2);
+	}
 ?>
