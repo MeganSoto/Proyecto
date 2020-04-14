73e10d3d3fef025c79a68fb9a2cc44e4f64b12a5
MeganSoto <62619472+MeganSoto@users.noreply.github.com>
2020-04-13 21:58:18 -0800

    Ultimos arreglos en variables de proceso.

--- proceso.php
+++ proceso.php
@@ -3,19 +3,23 @@
     $valor2= $_POST['n2'];
     $op=$_POST['operacion'];
 
-    if($op==1){
-        ECHO "la suma es: ".($valor1+$valor2);
+    if($op==1)
+	{
+        echo "La suma es: ".($valor1+$valor2);
 	}
 
-    if($op==2){
-        ECHO "la resta es: ".($valor1-$valor2);
+    if($op==2)
+	{
+        echo "La resta es: ".($valor1-$valor2);
 	}
 
-    if($op==3){
-        ECHO "la multiplicacion es: ".($valor1*$valor2);
+    if($op==3)
+	{
+        echo "La multiplicacion es: ".($valor1*$valor2);
 	}
 
-    if($op==4){
-        ECHO "la division es: ".($valor1/$valor2);
+    if($op==4)
+	{
+        echo "La division es: ".($valor1/$valor2);
 	}
 ?>
